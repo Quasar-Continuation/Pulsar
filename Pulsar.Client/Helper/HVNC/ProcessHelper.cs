4b9b30241b92b0ceb1b1dc6f531a0f4e4bf861a9
606 <coemsgg@proton.me>
2025-04-11 17:36:44 -0700

    Update ProcessHelper.cs

--- Pulsar.Client/Helper/HVNC/ProcessHelper.cs
+++ Pulsar.Client/Helper/HVNC/ProcessHelper.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Quasar.Client.Helper.HVNC
+namespace Pulsar.Client.Helper.HVNC
 {
     public class ProcessHelper
     {
